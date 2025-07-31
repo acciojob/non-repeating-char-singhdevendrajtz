88c9a2b2a7c7c9b9e03987f65398105eeb4b6682
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-07-31 14:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+
 function firstNonRepeatedChar(str) {
  // Write your code here
+	arr=[];
+	for(x of str)
+		{
+		expr=RegExp(x,'g');
+			if((str.match(expr)).length==1)
+			{
+				arr.push(x);
+			}
+		}
+	if(arr.length==0)
+	{
+		return(null);
+		
+	}
+	else 
+	{
+		return(arr);
+	}
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
